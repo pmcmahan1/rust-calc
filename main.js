641967fff792f1f8c6d5dc586dad773fd72a8335
Patrick McMahan <mcmahanconner@gmail.com>
2022-12-04 16:26:52 +0900

    Rough time

---
+++ main.js
@@ -0,0 +1,9 @@
+const input = document.querySelector('input');
+const woodenwall = document.getElementById('woodenwall');
+const stonewall = document.getElementById('stonewall');
+
+input.addEventListener('input', updateValue);
+
+function updateValue(e) {
+    console.log(e);
+  }
